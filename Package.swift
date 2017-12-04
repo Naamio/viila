d9555e004abd1dad62a6bfc7798855ba88e9dee7
Phil J. Laszkowicz <phil@fillip.pro>
2017-12-04 16:26:27 +0200

    Renamed ViilaFiles to ViilaSystem.

--- Package.swift
+++ Package.swift
@@ -8,21 +8,27 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Viila",
-            targets: ["ViilaFiles"]),
+            name: "ViilaSystem",
+            targets: ["ViilaSystem"]),
+        .library(
+            name: "ViilaWatch",
+            targets: ["ViilaWatch"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+         .target(
+            name: "ViilaSystem",
+            dependencies: []),
         .target(
-            name: "ViilaFiles",
+            name: "ViilaWatch",
             dependencies: []),
         .testTarget(
-            name: "ViilaFilesTests",
-            dependencies: ["ViilaFiles"]),
+            name: "ViilaSystemTests",
+            dependencies: ["ViilaSystem"]),
+        .testTarget(
+            name: "ViilaWatchTests",
+            dependencies: ["ViilaWatch"])
     ]
 )
